8fca27a3412009f64b893d98a4969de3008e0395
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-07-05 11:01:52 -0700

    added 404 route

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.get('/', function (req, res){
     console.log('client just hit the route')
     res.send('Hello World')
 })
+app.get('*', (req, res)=>{
+    res.status(404).send('<h1>404 page</h1>')
+})
 
 //Listen for connections.
 app.listen(process.env.PORT, ()=>{
