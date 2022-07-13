fd3bc3e4ce735fafa5257267b608f53fd91c210a
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-07-12 21:10:17 -0700

    added food pic to home page

--- views/home.jsx
+++ views/home.jsx
@@ -1,15 +1,20 @@
 const React = require('react')
 const Def = require('./default')
-//Create a home stub function 
 
+
+//Create a home stub function 
 function home(){
     return (
         <Def>
             <main>
-               
-                <h1>HOME</h1>
-                
-                
+               <h1>HOME</h1>
+                    <div>
+                        <img src="/images/food-homepage" alt='Chia Fruit Shake' />
+                        <div>
+                            Photo by <a href="AUTHOR_LINK">Brenda Godinez</a>on <a href="UNSPLASH_LINK">Unsplash</a>
+                        </div>
+                    </div>
+
                 <a href ="/places">
                     <button className="btn-primary">Places Page</button>
                 </a>
