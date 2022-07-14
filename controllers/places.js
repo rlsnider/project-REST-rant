4c4a887bfffb8510d63b7cae31314f755fd6c328
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-07-13 17:19:21 -0700

    worked on some bugs

--- controllers/places.js
+++ controllers/places.js
@@ -1,15 +1,16 @@
 const router = require('express').Router()
-const places = require('../models/places')
+const places = require('../models')
+
+//This will show a list of all "places"
+router.get('/', (req, res) => {
+  res.render('places/index', { places })
+ })
 
 //get places new
 router.get('/new', (req,res)=> {
     res.render('places/new')
 })
 
-//This will show a list of all "places"
- router.get('/', (req, res) => {
-  res.render('places/index', { places })
- })
 
 // show route
 router.get('/:id', (req, res)=>{
@@ -42,6 +43,7 @@ router.post('/', (req, res) => {
     places.push(req.body)
     res.redirect('/places')
   })
+
   //edit route
   router.get('/:id/edit', (req, res) => {
     let id = Number(req.params.id)
@@ -86,14 +88,18 @@ router.post('/', (req, res) => {
   
   
   //delete route
-  router.delete('/places/:id', (req, res)=>{
+  router.delete('/:id', (req, res)=>{
     let id = Number(req.params.id)
     if (isNaN(id)){
       res.render('error404')
     }
+    else if(!places[id]) {
+      res.render('error404')
+    }
     else{
       places.splice(id, 1)
-      res.redirect('places')
+      res.redirect('/places')
+
     }
   })
   
