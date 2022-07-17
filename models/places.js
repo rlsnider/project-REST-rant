91eca3056b38e230b15138d04eace778a1c298da
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-07-17 04:17:39 -0700

    started adding mongoose

--- models/places.js
+++ models/places.js
@@ -1,17 +1,12 @@
+const mongoose = require('mongoose')
 
-let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle', 
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: 'http://placekitten.com/250/250'
-}, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic:'public/images/Coding-cat-cafe'
-}]
-  
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded: Number
+})
 
-module.exports = places;
+module.exports = mongoose.model('Place', placeSchema)
