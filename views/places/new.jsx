994cd3efec5eb83c49adcbc61086558de5adb0ce
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-07-23 22:08:22 -0700

    Added error messages to min or max year

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -2,11 +2,18 @@ const React = require('react');
 const Def = require('../default')
 
 //new form
-function new_form() {
-    return(
+function new_form(data) {
+    let message =''
+        if (data.message) {
+            message = (
+                <h4 className="alert-danger">{data.message}</h4>
+            )
+        }
+    return (
         <Def>
             <main>
                 <h1>Add a New Place</h1>
+                {message}
                 <form method= "POST" action="/places">
                     <div className="form-group">
                         <label htmlFor="name">Place Name</label>
@@ -30,7 +37,7 @@ function new_form() {
                     </div>
                     <div className="form-group">
                         <label htmlFor="founded">Founded Year</label>
-                        <input type="number" className="form-control" id="founded" name="founded" value={new Date().getFullYear()} />
+                        <input type="number" className="form-control" id="founded" name="founded" defaultValue={new Date().getFullYear()} />
                     </div>
                     <div>
                         <input className="btn btn-primary" type="submit" value="Add Place" />
