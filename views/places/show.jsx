c9a2aceb9bd444431e7e54bb85c7dd8e06d3e566
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-07-23 21:09:03 -0700

    added and used custom method in show page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,19 +5,29 @@ function show (data) {
     return (
         <Def>
           <main>
-            <div>
-               {data.place.pic} 
-            <h1>{ data.place.name }</h1>
-                <h2>Rating</h2>
-                    <p>Not Rated</p>
-                <h2>Description</h2> 
-                <p>Located in {data.place.city}, {data.place.state} and serves {data.place.cuisines}</p>      
-            </div>
-            <div>
-                <h2>Comments</h2>
-                <p>No comments yet!</p>
-            </div>
-           
+            <div className="row">
+                <div className="col-sm-6">
+                  <img src={data.place.pic} alt ={data.place.name}/>
+                  <h3>
+                    Located in {data.place.city}, {data.place.state}
+                  </h3>
+                </div>
+                <div className="col-sm-6">
+                    <h2>Rating</h2>
+                        <p>Not Rated</p>  
+                    <h2>Description</h2> 
+                    <h3>
+                        {data.place.showEstablished()}
+                    </h3>
+                    <h4>
+                        Serving {data.place.cuisines}
+                    </h4>   
+                </div>
+                <div>
+                    <h2>Comments</h2>
+                    <p>No comments yet!</p>
+                </div>
+           </div>
              
             <a href={`/places/${data.id}/edit`} className="btn"> 
                 Edit
